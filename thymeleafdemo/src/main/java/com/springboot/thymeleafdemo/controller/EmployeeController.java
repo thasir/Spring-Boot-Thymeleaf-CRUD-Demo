68262a8e4a1322255992bae7afcf93229796e4be
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2021-01-02 20:44:58 +0530

    Spring Boot Thymeleaf CRUD - Add Employee

--- thymeleafdemo/src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo/src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springboot.thymeleafdemo.entity.Employee;
@@ -24,6 +26,22 @@ public class EmployeeController {
 	public String listEmployees(Model theModel) {
 		List<Employee> theEmployees = employeeService.findAll();
 		theModel.addAttribute("employees", theEmployees);
-		return "list-employees";
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		theModel.addAttribute("employee", theEmployee);
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		// save the employee
+		employeeService.save(theEmployee);
+		// use a redirect to prevent the duplicate submission
+		return "redirect:/employees/list";
 	}
 }
