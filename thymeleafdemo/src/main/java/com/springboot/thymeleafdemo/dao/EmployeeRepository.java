1837da51dcfaa9835172d29e2b41371003b4b0d4
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2021-01-02 20:49:19 +0530

    Spring Boot Thymeleaf sort by Last Name

--- thymeleafdemo/src/main/java/com/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ thymeleafdemo/src/main/java/com/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.thymeleafdemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	// thats it
+
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
